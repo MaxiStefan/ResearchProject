28bdef75e11b19d635e58c0d01239b5c77a7bc9d
maxifl <max@marxact.com>
2023-02-09 14:05:01 +0100

    small fix

--- start-wattsuppro.sh
+++ start-wattsuppro.sh
@@ -4,8 +4,10 @@ TreatmentTimeout=$(($(head -n 2 ./Containers/.env | tail -1 | cut -d '=' -f 2) *
 LogsExtractionTimeout=$(head -n 3 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 ServiceStartTimeout=$(head -n 4 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 SudoPassword=$(echo $(head -n 5 ./Containers/.env | tail -1 | cut -d '=' -f 2))
-
-LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtractionTimeout + $ServiceStartTimeout ) * 5 * 10 + 5 ))
+StartupTime=20
+TreatmentCount=5
+ExperimentIterations=10
+LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtractionTimeout + $ServiceStartTimeout ) * $TreatmentCount * $ExperimentIterations + $StartupTime ))
 
 echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl3.log -p /dev/ttyUSB2 -t $LoggerDuration > /dev/null 2>&1 &
 echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 -t $LoggerDuration > /dev/null 2>&1 &

915c990f5bdd95dfc808f123685f4a2b34431b61
maxifl <max@marxact.com>
2023-02-09 22:16:40 +0100

    cleanup

--- clean.sh
+++ clean.sh
@@ -4,57 +4,14 @@ treatments[1]="UP_IDLE"
 treatments[2]="IDLE"
 treatments[3]="FIBO_MATRIX"
 treatments[4]="BUBBLE_COLLATZ"
+SudoPassword=maximus
 
-sudo rm -rf Experiment_Iteration_1/$treatments[0]/powerapi/.cache Experiment_Iteration_1/$treatments[0]/powerapi/.local Experiment_Iteration_1/$treatments[0]/powerapi/.bashrc Experiment_Iteration_1/$treatments[0]/powerapi/.profile Experiment_Iteration_1/$treatments[0]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_1/$treatments[1]/powerapi/.cache Experiment_Iteration_1/$treatments[1]/powerapi/.local Experiment_Iteration_1/$treatments[1]/powerapi/.bashrc Experiment_Iteration_1/$treatments[1]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_1/$treatments[2]/powerapi/.cache Experiment_Iteration_1/$treatments[2]/powerapi/.local Experiment_Iteration_1/$treatments[2]/powerapi/.bashrc Experiment_Iteration_1/$treatments[2]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_1/$treatments[3]/powerapi/.cache Experiment_Iteration_1/$treatments[3]/powerapi/.local Experiment_Iteration_1/$treatments[3]/powerapi/.bashrc Experiment_Iteration_1/$treatments[3]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_1/$treatments[4]/powerapi/.cache Experiment_Iteration_1/$treatments[4]/powerapi/.local Experiment_Iteration_1/$treatments[4]/powerapi/.bashrc Experiment_Iteration_1/$treatments[4]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_2/$treatments[0]/powerapi/.cache Experiment_Iteration_2/$treatments[0]/powerapi/.local Experiment_Iteration_2/$treatments[0]/powerapi/.bashrc Experiment_Iteration_2/$treatments[0]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_2/$treatments[1]/powerapi/.cache Experiment_Iteration_2/$treatments[1]/powerapi/.local Experiment_Iteration_2/$treatments[1]/powerapi/.bashrc Experiment_Iteration_2/$treatments[1]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_2/$treatments[2]/powerapi/.cache Experiment_Iteration_2/$treatments[2]/powerapi/.local Experiment_Iteration_2/$treatments[2]/powerapi/.bashrc Experiment_Iteration_2/$treatments[2]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_2/$treatments[3]/powerapi/.cache Experiment_Iteration_2/$treatments[3]/powerapi/.local Experiment_Iteration_2/$treatments[3]/powerapi/.bashrc Experiment_Iteration_2/$treatments[3]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_2/$treatments[4]/powerapi/.cache Experiment_Iteration_2/$treatments[4]/powerapi/.local Experiment_Iteration_2/$treatments[4]/powerapi/.bashrc Experiment_Iteration_2/$treatments[4]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_3/$treatments[0]/powerapi/.cache Experiment_Iteration_3/$treatments[0]/powerapi/.local Experiment_Iteration_3/$treatments[0]/powerapi/.bashrc Experiment_Iteration_3/$treatments[0]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_3/$treatments[1]/powerapi/.cache Experiment_Iteration_3/$treatments[1]/powerapi/.local Experiment_Iteration_3/$treatments[1]/powerapi/.bashrc Experiment_Iteration_3/$treatments[1]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_3/$treatments[2]/powerapi/.cache Experiment_Iteration_3/$treatments[2]/powerapi/.local Experiment_Iteration_3/$treatments[2]/powerapi/.bashrc Experiment_Iteration_3/$treatments[2]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_3/$treatments[3]/powerapi/.cache Experiment_Iteration_3/$treatments[3]/powerapi/.local Experiment_Iteration_3/$treatments[3]/powerapi/.bashrc Experiment_Iteration_3/$treatments[3]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_3/$treatments[4]/powerapi/.cache Experiment_Iteration_3/$treatments[4]/powerapi/.local Experiment_Iteration_3/$treatments[4]/powerapi/.bashrc Experiment_Iteration_3/$treatments[4]/powerapi/.profile
-
-sudo rm -rf Experiment_Iteration_4/$treatments[0]/powerapi/.cache Experiment_Iteration_4/$treatments[0]/powerapi/.local Experiment_Iteration_4/$treatments[0]/powerapi/.bashrc Experiment_Iteration_4/$treatments[0]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_4/$treatments[1]/powerapi/.cache Experiment_Iteration_4/$treatments[1]/powerapi/.local Experiment_Iteration_4/$treatments[1]/powerapi/.bashrc Experiment_Iteration_4/$treatments[1]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_4/$treatments[2]/powerapi/.cache Experiment_Iteration_4/$treatments[2]/powerapi/.local Experiment_Iteration_4/$treatments[2]/powerapi/.bashrc Experiment_Iteration_4/$treatments[2]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_4/$treatments[3]/powerapi/.cache Experiment_Iteration_4/$treatments[3]/powerapi/.local Experiment_Iteration_4/$treatments[3]/powerapi/.bashrc Experiment_Iteration_4/$treatments[3]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_4/$treatments[4]/powerapi/.cache Experiment_Iteration_4/$treatments[4]/powerapi/.local Experiment_Iteration_4/$treatments[4]/powerapi/.bashrc Experiment_Iteration_4/$treatments[4]/powerapi/.profile
-
-sudo rm -rf Experiment_Iteration_5/$treatments[0]/powerapi/.cache Experiment_Iteration_5/$treatments[0]/powerapi/.local Experiment_Iteration_5/$treatments[0]/powerapi/.bashrc Experiment_Iteration_5/$treatments[0]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_5/$treatments[1]/powerapi/.cache Experiment_Iteration_5/$treatments[1]/powerapi/.local Experiment_Iteration_5/$treatments[1]/powerapi/.bashrc Experiment_Iteration_5/$treatments[1]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_5/$treatments[2]/powerapi/.cache Experiment_Iteration_5/$treatments[2]/powerapi/.local Experiment_Iteration_5/$treatments[2]/powerapi/.bashrc Experiment_Iteration_5/$treatments[2]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_5/$treatments[3]/powerapi/.cache Experiment_Iteration_5/$treatments[3]/powerapi/.local Experiment_Iteration_5/$treatments[3]/powerapi/.bashrc Experiment_Iteration_5/$treatments[3]/powerapi/.profile
-sudo rm -rf Experiment_Iteration_5/$treatments[4]/powerapi/.cache Experiment_Iteration_5/$treatments[4]/powerapi/.local Experiment_Iteration_5/$treatments[4]/powerapi/.bashrc Experiment_Iteration_5/$treatments[4]/powerapi/.profile
-
-sudo rm -rf Experiment_Iteration_1/$treatments[1]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_1/$treatments[2]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_1/$treatments[3]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_1/$treatments[4]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_2/$treatments[0]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_2/$treatments[1]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_2/$treatments[2]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_2/$treatments[3]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_2/$treatments[4]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_3/$treatments[0]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_3/$treatments[1]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_3/$treatments[2]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_3/$treatments[3]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_3/$treatments[4]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_4/$treatments[0]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_4/$treatments[1]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_4/$treatments[2]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_4/$treatments[3]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_4/$treatments[4]/powerapi/.bash_logout
-
-sudo rm -rf Experiment_Iteration_5/$treatments[0]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_5/$treatments[1]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_5/$treatments[2]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_5/$treatments[3]/powerapi/.bash_logout
-sudo rm -rf Experiment_Iteration_5/$treatments[4]/powerapi/.bash_logout
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    for treatment in ${$treatments[@]}
+    do
+        cd Experiment_Iteration_$i/$treatment/powerapi
+        echo $SudoPassword | sudo -S rm -rf .local .cache
+        cd ..; cd ..; cd ..;
+    done
+done

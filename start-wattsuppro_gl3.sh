de12a951b188e32b6c6001749f215c3187eda477
Maximilian <maxi_stefan_1@yahoo.com>
2023-02-11 17:22:52 +0000

    wattsuppro script fixes

--- start-wattsuppro_gl3.sh
+++ start-wattsuppro_gl3.sh
@@ -4,15 +4,16 @@ TreatmentTimeout=$(($(head -n 2 ./Containers/.env | tail -1 | cut -d '=' -f 2) *
 LogsExtractionTimeout=$(head -n 3 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 ServiceStartTimeout=$(head -n 4 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 SudoPassword=$(echo $(head -n 5 ./Containers/.env | tail -1 | cut -d '=' -f 2))
-StartupTime=20
+ResidualLoggingTime=18000
 TreatmentCount=5
 ExperimentIterations=10
-LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtractionTimeout + $LogsExtractionTimeout + $ServiceStartTimeout ) * $TreatmentCount * $ExperimentIterations + $StartupTime ))
+LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtractionTimeout + $LogsExtractionTimeout + $ServiceStartTimeout ) * $TreatmentCount * $ExperimentIterations + $ResidualLoggingTime ))
 #LoggerDuration=120
 
 echo "Started logging at $(date "+%T.%6N")"
 
-echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl3.log -p /dev/ttyUSB2 -t $LoggerDuration
-#echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 -t $LoggerDuration
+echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl3.log -p /dev/ttyUSB2 -t $LoggerDuration > /dev/null 2>&1 &
+
+sleep $LoggerDuration
 
 echo "Stopped logging at $(date "+%T.%6N")"
--- start-wattsuppro_gl4.sh
+++ start-wattsuppro_gl4.sh
@@ -4,7 +4,7 @@ TreatmentTimeout=$(($(head -n 2 ./Containers/.env | tail -1 | cut -d '=' -f 2) *
 LogsExtractionTimeout=$(head -n 3 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 ServiceStartTimeout=$(head -n 4 ./Containers/.env | tail -1 | cut -d '=' -f 2)
 SudoPassword=$(echo $(head -n 5 ./Containers/.env | tail -1 | cut -d '=' -f 2))
-ResidualLoggingTime=3600
+ResidualLoggingTime=18000
 TreatmentCount=5
 ExperimentIterations=10
 LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtractionTimeout + $LogsExtractionTimeout + $ServiceStartTimeout ) * $TreatmentCount * $ExperimentIterations + $ResidualLoggingTime ))
@@ -12,7 +12,7 @@ LoggerDuration=$(( ($TreatmentTaskDuration + $TreatmentTimeout + $LogsExtraction
 
 echo "Started logging at $(date "+%T.%6N")"
 
-#echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl3.log -p /dev/ttyUSB2 -t $LoggerDuration > /dev/null 2>&1 &
-echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 -t $LoggerDuration
+echo $SudoPassword | sudo -S python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 -t $LoggerDuration > /dev/null 2>&1 &
 
+sleep $LoggerDuration
 echo "Stopped logging at $(date "+%T.%6N")"
